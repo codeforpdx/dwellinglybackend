50e092a254cf67b739df6515dc2a4988ca587b62
Dave Trost <davetrost@gmail.com>
2020-06-24 22:16:27 -0700

    improved handling for SIGINT in view_coverage.py
    Use signal library to listen for ctrl-C or kill <process> and gracefully exit the html coverage server

--- view_coverage.py
+++ view_coverage.py
@@ -1,11 +1,17 @@
 import http.server
 import socketserver
 import subprocess
+import signal
+import sys
 
 result = subprocess.call(["pipenv", "run", "coverage", "html"])
 if(result): 
     print("There was an error generating the HTML coverage report. Exiting.")
     exit()
+
+def stop_coverage(sig, frame):  
+    sys.exit(0)
+signal.signal(signal.SIGINT, stop_coverage)
 
 # the approach below is from: https://2ality.com/2014/06/simple-http-server.html
 PORT = 8001
