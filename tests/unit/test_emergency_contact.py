22b4c5ecb3e73a2fa2118b29a314821178df78e1
Spenser Saling <spenser0saling@gmail.com>
2020-07-12 16:44:26 -0700

    Add comments

--- tests/unit/test_emergency_contact.py
+++ tests/unit/test_emergency_contact.py
@@ -6,11 +6,10 @@ contact_number = "503-291-9111"
 contact_numtype = "Call"
 
 def test_emergency_contact(app):
+  """The properties must match the model's inputs."""
   with app.app_context():
     emergency_contact = EmergencyContactModel(name=emergency_contact_name, contact_numbers=[{"number": contact_number, "numtype": contact_numtype}], description=emergency_contact_description)
     assert emergency_contact.name == emergency_contact_name
     assert emergency_contact.description == emergency_contact_description
     assert emergency_contact.contact_numbers[0].number == contact_number
     assert emergency_contact.contact_numbers[0].numtype == contact_numtype
-
-
--- tests/unit/test_user.py
+++ tests/unit/test_user.py
@@ -4,7 +4,7 @@ adminUserEmail = "user1@dwellingly.org"
 adminRole = "admin"
 
 def test_admin_user():
-    """The properties must match those sent to the user model."""
+    """The properties must match the model's inputs."""
     admin_user = UserModel(email=adminUserEmail, password="1234", firstName="user1", lastName="admin", role=adminRole, archived=0)
     assert admin_user.email == adminUserEmail
     assert admin_user.role == adminRole
