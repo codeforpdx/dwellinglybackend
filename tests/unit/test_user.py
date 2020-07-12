f310ad7940b42c5afa6c77514fa739697fd9ddda
Spenser Saling <spenser0saling@gmail.com>
2020-07-12 16:35:11 -0700

    Small refactor to user unit test

--- tests/unit/test_user.py
+++ tests/unit/test_user.py
@@ -1,7 +1,8 @@
-from conftest import adminUserEmail
-from conftest import adminRole
+adminUserEmail = "user1@dwellingly.org"
+adminRole = "admin"
 
-def test_admin_user(admin_user):
+def test_admin_user():
     """The properties that match those sent to the user model by the fixture 'admin_user'."""
+    adminUser = UserModel(email=adminUserEmail, password=userPassword, firstName="user1", lastName="admin", role=adminRole, archived=0)
     assert admin_user.email == adminUserEmail
     assert admin_user.role == adminRole
