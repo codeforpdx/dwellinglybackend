b505b891ced6397830a2351c7c377b60716ecaee
Zachary Fregin <zach.fregin@gmail.com>
2020-07-13 21:31:50 -0700

    Revert "Merge branch 'development' into travis-ci"
    This reverts commit c41e14a336ea69959683ea65530a0d9c5d61c444, reversing
    changes made to b8dfc81d858f2e9b17410d8290489ec8903a2940.

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager, jwt_refresh_token_required, create_access_token, get_jwt_identity
 from flask_cors import CORS
-from flask_mail import Mail
 from resources.admin_required import admin_required
 from models.user import UserModel
 from models.property import PropertyModel
@@ -19,7 +18,9 @@ from resources.tickets import Ticket, Tickets
 import os
 from db import db
 
-def config_app(app):
+def create_app():
+    app = Flask(__name__)
+
     #config DataBase
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", default = 'sqlite:///data.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -51,6 +52,11 @@ def config_app(app):
     app.config['MAIL_SUPPRESS_SEND'] = False #same as testing
     app.config['MAIL_ASCII_ATTACHMENTS'] = False
 
+    #allow cross-origin (CORS)
+    CORS(app)
+
+    db.init_app(app) #need to solve this
+    return app
 
 def create_routes(app):
     api = Api(app, prefix="/api/")
@@ -69,13 +75,17 @@ def create_routes(app):
     api.add_resource(Tickets, '/tickets')
     api.add_resource(Ticket, '/tickets/<int:id>')
 
+app = create_app()
+api = Api(app, prefix="/api/")
 
+@app.before_first_request
 def check_for_admins():
     errorMsg = (
-        "\n\n=-=-=-=-=-=-=-=\n"
-        "WARNING! Database unusable. Did you forget to run the seed_db.py script? `python seed_db.py`"
-        "\n=-=-=-=-=-=-=-=\n\n"
+    "\n\n=-=-=-=-=-=-=-=\n"
+    "WARNING! Database unusable. Did you forget to run the seed_db.py script? `python seed_db.py`"
+    "\n=-=-=-=-=-=-=-=\n\n"
     )
+
     assert (os.path.isfile('./data.db')), errorMsg
     try:
         admins = UserModel.find_by_role('admin')
@@ -85,48 +95,46 @@ def check_for_admins():
         if(not len(admins)):
             print(errorMsg)
 
+jwt = JWTManager(app) # /authorization
 
-def create_app():
-    app = Flask(__name__)
-
-    #configure the flask settings
-    config_app(app)
-
-    #declare the available routes
-    create_routes(app)
-
-    #allow cross-origin (CORS)
-    CORS(app)
-
-    # set up authorization
-    app.jwt = JWTManager(app) 
-
-    # initialize Mail
-    app.mail = Mail(app)
-    
-    # ensure the database has been initialized (development only)
-    @app.before_first_request
-    def decorated_check_for_admins(): check_for_admins()
-
-    # check the user role in the JSON Web Token (JWT)
-    @app.jwt.user_claims_loader
-    def role_loader(identity): 
-        user = UserModel.find_by_id(identity)
-        return {'email': user.email, 'firstName': user.firstName, 'lastName': user.lastName, 'is_admin': (user.role == 'admin')}
-
-    # checking if the token's jti (jwt id) is in the set of revoked tokens
-    # this check is applied globally (to all routes that require jwt)
-    @app.jwt.token_in_blacklist_loader
-    def check_if_token_in_blacklist(decrypted_token):
-        jti = decrypted_token['jti']
-        return RevokedTokensModel.is_jti_blacklisted(jti)
-
-    db.init_app(app)
-    return app
+mail = Mail(app) #init Mail
 
 
-app = create_app()
+@jwt.user_claims_loader
+#check if user role == admin
+def role_loader(identity): #identity = user.id in JWT
+    user = UserModel.find_by_id(identity)
+    return {
+        'email': user.email,
+        'firstName': user.firstName,
+        'lastName': user.lastName,
+        'is_admin': (user.role == 'admin')
+    }
+
+# checking if the token's jti (jwt id) is in the set of revoked tokens
+# this check is applied globally (to all routes that require jwt)
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    return RevokedTokensModel.is_jti_blacklisted(jti)
+
+
+api.add_resource(UserRegister, '/register')
+api.add_resource(Property,'/properties/<string:name>') #TODO change to ID
+api.add_resource(Properties,'/properties')
+api.add_resource(ArchiveProperty,'/properties/archive/<int:id>')
+api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UsersRole, '/users/role')
+api.add_resource(ArchiveUser, '/user/archive/<int:user_id>')
+api.add_resource(UserLogin, '/login')
+api.add_resource(Email, '/user/message')
+api.add_resource(UserAccessRefresh, '/refresh')
+api.add_resource(Tenants, '/tenants', '/tenants/<int:tenant_id>')
+api.add_resource(Tickets, '/tickets')
+api.add_resource(Ticket, '/tickets/<int:id>')
+api.add_resource(EmergencyContacts, '/emergencycontacts', '/emergencycontacts/<int:id>')
 
 
 if __name__ == '__main__':
+    # db.init_app(app)
     app.run(port=5000, debug=True)
--- resources/user.py
+++ resources/user.py
@@ -38,8 +38,13 @@ class UserRegister(Resource):
         return {"message": "User created successfully."}, 201
 
 class User(Resource):
-    @admin_required
+    @jwt_required
     def get(self, user_id):
+        #check if is_admin exist if not discontinue function
+        claims = get_jwt_claims()         
+        if not claims['is_admin']:
+            return {'message': 'Admin Access Required'}, 401
+
         user = UserModel.find_by_id(user_id)
 
         if not user:
@@ -55,35 +60,72 @@ class User(Resource):
         return user_info, 200
 
     @admin_required
+        # hard coded return as .json() is not compatiable with user model and sqlalchemy
+        return {
+            'id': str(user.id),
+            'firstName': user.firstName,
+            'lastName': user.lastName,
+            'email': user.email,
+            'role': user.role,
+            'archived': user.archived
+        }, 200
+        
+    @jwt_required
     def patch(self,user_id):
+        #check if is_admin exists - if not discontinue function
+        claims = get_jwt_claims()         
+        if not claims['is_admin']:
+            return {'message': "Admin Access Required"}, 401
+
         parser = reqparse.RequestParser()
         parser.add_argument('role',type=str,required=True,help="This field cannot be blank.")
 
         user = UserModel.find_by_id(user_id)
         if not user:
-            return {"Message": "Unable to update user"}, 400
+            return {"Message": "Unable to update user"}
 
         data = parser.parse_args()
         user.role = data['role']
         try:
             user.save_to_db()
         except:
-            return {'Message': 'An Error Has Occurred'}, 500
+            return {'Message': 'An Error Has Occured'}, 500
 
         return user.json(), 201
 
-    @admin_required
+    @jwt_required
     def delete(self, user_id):
+        #check if is_admin exists - if not discontinue function
+        claims = get_jwt_claims()         
+        if not claims['is_admin']:
+            return {'message': "Admin Access Required"}, 401
+
         user = UserModel.find_by_id(user_id)
         if not user:
-            return {"Message": "Unable to delete User"}, 400
+            return {"Message", "Unable to delete User"}, 404 
         user.delete_from_db()
         return {"Message": "User deleted"}, 200
 
+#pull all users - for debugging purposes disable before production
+class Users(Resource):
+    @jwt_required
+    def get(self):
+        #check if is_admin exists - if not discontinue function
+        claims = get_jwt_claims() 
+        if not claims['is_admin']:
+            return {'message': "Admin Access Required"}, 401
+        return {'Users': [user.json() for user in UserModel.query.all()]}
+
 class ArchiveUser(Resource):
 
-    @admin_required
+    @jwt_required
     def post(self, user_id):
+        #check if is_admin exist if not discontinue function
+        claims = get_jwt_claims() 
+
+        if not claims['is_admin']:
+            return {'message': "Admin Access Required"}, 401
+
         user = UserModel.find_by_id(user_id)
         if(not user):
             return{'Message': 'User cannot be archived'}, 400
@@ -129,8 +171,13 @@ class UsersRole(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('userrole',type=str,required=True,help="This field cannot be blank.")
 
-    @admin_required
+    @jwt_required
     def post(self):
+        #check if is_admin exist if not discontinue function
+        claims = get_jwt_claims() 
+        if not claims['is_admin']:
+            return {'message': "Admin Access Required"}, 401
+
         data = UsersRole.parser.parse_args()
         users = UserModel.find_by_role(data['userrole'])
         users_info = []
