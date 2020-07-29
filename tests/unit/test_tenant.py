4d210b88a8e6f76ce57875d5dbd1f99fc2bfdbd3
Spenser Saling <spenser0saling@gmail.com>
2020-07-29 15:57:59 -0700

    Cleanup formatting and unused imports

--- tests/unit/test_tenant.py
+++ tests/unit/test_tenant.py
@@ -6,8 +6,10 @@ phone = "800-RENT-ALOT"
 propertyID = 1
 staffIDs = [1, 2]
 
+
 def test_tenant(test_database):
-    newTenant = TenantModel(firstName=firstName, lastName=lastName, phone=phone, propertyID=propertyID, staffIDs=staffIDs)
+    newTenant = TenantModel(firstName=firstName, lastName=lastName,
+                            phone=phone, propertyID=propertyID, staffIDs=staffIDs)
     assert newTenant.firstName == firstName
     assert newTenant.lastName == lastName
     assert newTenant.phone == phone
