66643c6d519e11f1a62f3ba88f54c55592bb765f
Spenser Saling <spenser0saling@gmail.com>
2020-07-29 15:38:39 -0700

    Add tenant tests

---
+++ tests/unit/test_tenant.py
@@ -0,0 +1,14 @@
+from models.tenant import TenantModel
+
+firstName = "Renty"
+lastName = "McRenter"
+phone = "800-RENT-ALOT"
+propertyID = 1
+staffIDs = [1, 2]
+
+def test_tenant(test_database):
+    newTenant = TenantModel(firstName=firstName, lastName=lastName, phone=phone, propertyID=propertyID, staffIDs=staffIDs)
+    assert newTenant.firstName == firstName
+    assert newTenant.lastName == lastName
+    assert newTenant.phone == phone
+    assert newTenant.propertyID == propertyID
