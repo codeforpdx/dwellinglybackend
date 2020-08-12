f68a8a3e18a3b39094d26c1293220c4eff949a2f
Cecille Salazar <cecillersalazar@gmail.com>
2020-08-11 20:14:22 -0700

    Added properties test for notes model

--- tests/unit/test_ticket.py
+++ tests/unit/test_ticket.py
@@ -1,7 +1,8 @@
 from models.tickets import TicketModel
+from models.notes import NotesModel
 
 def test_ticket():
-    """The properties must match the model's inputs."""
+    """The properties must match the ticket model's inputs."""
     testIssue = "Property Damage"
     testSender = "user1 tester"
     testTenant = "Renty McRenter"
@@ -24,3 +25,21 @@ def test_ticket():
     assert test_ticket.status == testStatus
     assert test_ticket.urgency == testUrgency
     assert test_ticket.assignedUser == testAssignedUser
+
+
+def test_notes():
+    """The properties must match the notes model's inputs."""
+
+    testTicketId = 1
+    testText = "Tenant has 40 cats"
+    testUser = "user1 tester"
+
+    test_note = NotesModel(
+        ticketid=testTicketId,
+        text=testText,
+        user=testUser,
+    )
+
+    assert test_note.ticketid == testTicketId
+    assert test_note.text == testText
+    assert test_note.user == testUser
