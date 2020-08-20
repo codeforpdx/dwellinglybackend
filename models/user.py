0cf13d6e07b7b0ea6e96a0e465d972526b35830c
htharker42 <htharker@gmail.com>
2020-08-19 21:31:11 -0700

    updated users and widgets

--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta
 from db import db
 
 class UserModel(db.Model):
@@ -13,7 +13,8 @@ class UserModel(db.Model):
     phone = db.Column(db.String(25))
     password = db.Column(db.String(128))
     archived = db.Column(db.Boolean)
-    lastActive = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    lastActive = db.Column(db.DateTime, default=datetime.utcnow)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
 
 
     def __init__(self, firstName, lastName, email, password, phone, role, archived):
@@ -24,10 +25,11 @@ class UserModel(db.Model):
         self.password = password
         self.role = role if role else 'pending'
         self.archived = False
-        self.lastActive = datetime.datetime.utcnow()
+        self.lastActive = datetime.utcnow()
+        self.created = datetime.utcnow()
 
     def update_last_active(self):
-        self.lastActive = datetime.datetime.utcnow()
+        self.lastActive = datetime.utcnow()
         db.session.commit()
 
     def save_to_db(self):
@@ -46,10 +48,19 @@ class UserModel(db.Model):
             'email': self.email,
             'phone': self.phone,
             'role': self.role,
+            'created': self.created.strftime('%Y-%m-%d %H:%M:%S %Z'),
             'archived': self.archived,
-            'lastActive': self.lastActive.astimezone(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S %Z')
+            'lastActive': self.lastActive.strftime('%Y-%m-%d %H:%M:%S %Z')
         }
-
+    
+    def widgetJson(self):
+        # "{} {}".format(self.firstName, self.lastName)
+        return{
+            'id': "self.id",
+            'stat': 'Today',
+            'desc': "test",
+            'subtext': "null"
+        }
 
     @classmethod
     def find_by_email(cls, email):
@@ -62,3 +73,10 @@ class UserModel(db.Model):
     @classmethod
     def find_by_role(cls, role):
         return cls.query.filter_by(role=role).all()
+    
+    @classmethod
+    def find_recent_role(cls, role, days):
+        dateTime = datetime.now() - timedelta(days = days)
+        return db.session.query(UserModel).filter(UserModel.role == role).order_by(UserModel.created.desc()).limit(3).all()
+        # return db.session.query(UserModel).filter(UserModel.created >= dateTime).filter(UserModel.role == role).order_by(UserModel.created.desc()).limit(4)
+
--- resources/widgets.py
+++ resources/widgets.py
@@ -2,16 +2,95 @@ from flask_restful import Resource, reqparse
 import json
 from models.tickets import TicketModel
 from models.user import UserModel
+from models.property import PropertyModel
 from flask_jwt_extended import jwt_required 
 from datetime import datetime
 
 class Widgets(Resource):
+    # @jwt_required
+   
     def get(self):
-        newTickets = TicketModel.find_count_by_age_status("New", 1440)
-        print(newTickets)
-        return {'New': TicketModel.find_count_by_status("New"), 
-        'Unseen24': TicketModel.find_count_by_age_status("New", 1440), 
-        'InProgress': TicketModel.find_count_by_status("In Progress"), 
-        'WeekOld': TicketModel.find_count_by_age_status("In Progress", 10080), 
-        'Closed':TicketModel.find_count_by_status("Closed"), 
-        'Compliments': 1}, 200
+        users = UserModel.find_recent_role("property-manager", 5)
+        projectManagers = []
+        for user in users:
+            print(user.widgetJson)
+            projectManagers.append(user)
+
+        return { 'opentickets':{
+            'title': 'Open Tickets', 
+            'stats': [[
+                {
+                    "stat": TicketModel.find_count_by_status("New"),
+                    "desc": 'New',
+                },
+                {
+                    "stat": TicketModel.find_count_by_age_status("New", 1440),
+                    "desc": "Unseen for > 24 hours",
+                }
+            ],
+            [
+                {
+                    "stat": TicketModel.find_count_by_status("In Progress"),
+                    "desc": 'In Progress'
+                },
+                {
+                    "stat": TicketModel.find_count_by_age_status("In Progress", 10080),
+                    "desc": 'In progress for > 1 week',
+                }
+            ]]
+        },
+        'reports':{
+            'title': 'Reports',
+            'link': '#',
+            'stats': [
+                [ 
+                    {
+                        'stat': 0,
+                        'desc': 'Compliments',
+                        'subtext': 'in the last week'
+                    },
+                ],
+                [
+                    {
+                        'stat': TicketModel.find_count_by_status("Closed"),
+                        'desc': "Closed tickets",
+                        'subtext': 'in the last week'
+                    }
+                ]
+            ]
+        },
+        'managers':{
+                'title': 'New Property Managers',
+                'link': '#',
+                'isDate': True,
+                'stats': [
+                    [ 
+                        {
+                            'stat': 'Today',
+                            'desc': 'Property Manager Name',
+                            'subtext': 'Meerkat Manor'
+                        },
+                        {
+                        'stat': '01/14',
+                        'desc': 'Property Manager Name',
+                        'subtext': 'Meerkat Manor'
+                        },
+                        {
+                            'stat': '02/04',
+                            'desc': 'Property Manager Name',
+                            'subtext': 'Meerkat Manor'
+                        },
+                    ],
+                ]
+            }
+        }, 200
+        
+        
+        
+        # {'New': TicketModel.find_count_by_status("New"), 
+        # 'Unseen24': TicketModel.find_count_by_age_status("New", 1440), 
+        # 'InProgress': TicketModel.find_count_by_status("In Progress"), 
+        # 'WeekOld': TicketModel.find_count_by_age_status("In Progress", 10080), 
+        # 'Closed':TicketModel.find_count_by_status("Closed"), 
+        # 'Compliments': 1}, 200
+
