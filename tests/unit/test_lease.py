9288ad92831b86c1952c512900a4b3f61c2a6fda
Nick Schimek <cfme00@gmail.com>
2020-08-26 17:25:18 +0900

    Test lease json method

--- tests/unit/test_lease.py
+++ tests/unit/test_lease.py
@@ -1,6 +1,23 @@
 from models.lease import LeaseModel
+from tests.time import Time
+from models.user import UserModel
+from models.property import PropertyModel
+from models.tenant import TenantModel
 
-def test_lease_initializes():
-    lease = LeaseModel(0, 0, 0, 0, 0, 0, 0, 0)
-    assert lease
+def test_json(test_database):
+    lease = LeaseModel.find_by_id(1)
+    property = PropertyModel.find_by_id(lease.propertyID)
+    landlord = UserModel.find_by_id(lease.landlordID)
+    tenant = TenantModel.find_by_id(lease.tenantID)
 
+    assert lease.json() == {
+            'id': lease.id,
+            'name': lease.name,
+            'propertyID': property.json(),
+            'landlordID': landlord.json(),
+            'tenantID': tenant.json(),
+            'dateTimeStart': Time.format_date(lease.dateTimeStart),
+            'dateTimeEnd': Time.format_date(lease.dateTimeEnd),
+            'dateUpdated': Time.format_date(lease.dateUpdated),
+            'occupants': lease.occupants
+        }
