0ec096555c80860970841ba8f4a24304fed2af33
Nick Schimek <cfme00@gmail.com>
2020-09-11 10:01:57 +0900

    Add test for when not to send email

--- tests/integration/test_forgot_password.py
+++ tests/integration/test_forgot_password.py
@@ -25,10 +25,19 @@ class TestForgotPassword:
         assert response.json == user.json()
 
     @patch.object(Email, 'send_reset_password_msg')
-    def test_password_reset_email_sent(self, stubbed_reset_password_method):
+    def test_reset_password_email_sent(self, stubbed_reset_password_method):
         user = UserModel.find_by_id(1)
 
         response = self.client.post('/api/forgot_password', json={'email': user.email})
 
         stubbed_reset_password_method.assert_called_once_with(user)
         assert is_valid(response, 200)
+
+    @patch.object(Email, 'send_reset_password_msg')
+    def test_reset_password_email_is_not_sent_when_invalid_user(self, stubbed_reset_password_method):
+        user = UserModel.find_by_id(1)
+
+        response = self.client.post('/api/forgot_password', json={'email': 'invalid_email@nickschimek.com'})
+
+        stubbed_reset_password_method.assert_not_called()
+        assert is_valid(response, 400)
