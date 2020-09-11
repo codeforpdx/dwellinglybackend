bf0c8a17409cf40fe4f2f00220ab51df269f68fe
Nick Schimek <cfme00@gmail.com>
2020-09-11 13:17:42 +0900

    Create reset password token

--- models/user.py
+++ models/user.py
@@ -1,7 +1,10 @@
 from datetime import datetime, timedelta
+import time
+import jwt
 from db import db
 from enum import Enum
 from models.base_model import BaseModel
+from flask import current_app
 
 class RoleEnum(Enum):
     PENDING = 0
@@ -42,7 +45,15 @@ class UserModel(BaseModel):
         self.lastActive = datetime.utcnow()
         db.session.commit()
 
-    def json(self): 
+    def reset_password_token(self):
+        ten_minutes = 600
+        return jwt.encode(
+                {'reset_password': self.id, 'exp': time.time() + ten_minutes},
+                current_app.secret_key,
+                algorithm='HS256'
+            ).decode('utf-8')
+
+    def json(self):
         return {
             'id': self.id,
             'firstName': self.firstName,
