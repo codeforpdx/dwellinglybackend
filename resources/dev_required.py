50462b3d8a7256dab7c64275b3a2843b59345b10
github username <souzajohnlee@gmail.com>
2020-09-15 14:39:40 -0700

    dev auth now required through user role verification

--- resources/dev_required.py
+++ resources/dev_required.py
@@ -1,12 +1,15 @@
 from flask_jwt_extended import jwt_required, get_jwt_claims
+from models.user import UserModel, RoleEnum
 
 def dev_required(func):
     @jwt_required
     def check_dev_wrapper(*args, **kwargs):
-        #check if is_dev exist. If not discontinue function
+        #check if user is dev, if not return 401
         claims = get_jwt_claims() 
+        user = UserModel.find_by_email(claims['email'])
+        print(user.role)
         print(get_jwt_claims())
-        if not claims['is_dev']:
+        if user.role != RoleEnum.DEV:
             return {'message': "Dev Access Required"}, 401
 
         return func(*args, **kwargs)
