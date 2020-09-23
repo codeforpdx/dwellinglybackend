e479d835525ba850a4726e3d171a4dd6e67d3d35
Cecille Salazar <cecillersalazar@gmail.com>
2020-09-22 22:01:32 -0700

    Removed type checking as non-strings are getting converted

--- utils/auth.py
+++ utils/auth.py
@@ -1,9 +1,7 @@
 import bcrypt
 
 def hash_pw(plaintext_password):
-    if isinstance(plaintext_password, str):
-        return bcrypt.hashpw(bytes(plaintext_password, 'utf-8'), bcrypt.gensalt())
+    return bcrypt.hashpw(bytes(plaintext_password, 'utf-8'), bcrypt.gensalt())
 
 def check_pw(plaintext_password, db_password):
-    if isinstance(plaintext_password, str):
-        return bcrypt.checkpw(bytes(plaintext_password, 'utf-8'), db_password)
+    return bcrypt.checkpw(bytes(plaintext_password, 'utf-8'), db_password)
