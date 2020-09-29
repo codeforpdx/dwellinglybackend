553f0de9c2e51234edea155f79556237c1b70078
Nick Schimek <cfme00@gmail.com>
2020-09-29 15:49:29 +0900

    Create base interface test (#114)
    * Create base interface test
    
    Created unit tests for the base model. This was implemented in a
    separate test file so that it can be used to test inherited classes
    confirm to the interface of the base model.
    
    Created a new method find. This is done to replace the find_by_id
    method, because the find method will automatically return a 404 status
    code and custom json message. This will reduce the number of
    conditionals needed in the endpoints and the number of integration tests
    needed.
    
    * Implement base interface test in lease model
    
    * Create delete method
    
    This method automatically returns a proper json response if the object
    being deleted is not in the database.
    
    * Remove 'Model' suffix from custom 404 msg

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,16 @@ class BaseModel(db.Model):
     def find_by_id(cls, id):
         return cls.query.filter_by(id=id).first()
 
+    @classmethod
+    def find(cls, id):
+        return cls.query.get_or_404(id, f'{cls._name()} not found')
+
+    @classmethod
+    def delete(cls, id):
+        obj = cls.find(id)
+        db.session.delete(obj)
+        db.session.commit()
+
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
@@ -16,5 +26,12 @@ class BaseModel(db.Model):
         db.session.delete(self)
         db.session.commit()
 
+    def json(self):
+        raise NotImplementedError(f"This {self.__class__} must implement a <json> method")
+
     def __repr__(self):
         return f'<{type(self).__name__} {self.json()}>'
+
+    @classmethod
+    def _name(cls):
+        return cls.__name__.replace('Model', '')
---
+++ tests/unit/base_interface_test.py
@@ -0,0 +1,41 @@
+from unittest.mock import patch
+from db import db
+
+
+class BaseInterfaceTest:
+    @patch.object(db, 'session')
+    def test_save_to_db(self, mock_session):
+        self.object.save_to_db()
+
+        mock_session.add.assert_called_with(self.object)
+        mock_session.commit.assert_called()
+
+    @patch.object(db, 'session')
+    def test_delete_from_db(self, mock_session):
+        self.object.delete_from_db()
+
+        mock_session.delete.assert_called_with(self.object)
+        mock_session.commit.assert_called()
+
+    @patch.object(db, 'session')
+    def test_delete(self, mock_session):
+        with patch.object(self.object.__class__, 'find', return_value=self.object) as mock_find:
+            self.object.__class__.delete(1)
+
+        mock_find.assert_called()
+
+        mock_session.delete.assert_called_with(self.object)
+        mock_session.commit.assert_called()
+
+    def test_find(self):
+        class_name = self.object.__class__.__name__
+        with patch.object(self.object.__class__, 'query') as mock_query:
+            self.object.__class__.find(1)
+
+        mock_query.get_or_404.assert_called_with(1, self.custom_404_msg)
+
+    def test_json(self):
+        with patch.object(self.object, 'json') as mock_json:
+            self.object.json()
+
+        mock_json.assert_called()
