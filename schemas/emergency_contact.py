64c53917055c5795ab550728beb7e4c22f400677
gheelan <gheelan0894@gmail.com>
2020-10-29 13:36:42 -0700

    Added files for emergency contact & contact number schemas and tests.

---
+++ schemas/emergency_contact.py
@@ -0,0 +1,28 @@
+from ma import ma
+from models.emergency_contact import EmergencyContactModel
+from models.contact_number import ContactNumberModel
+from utils.time import time_format
+from marshmallow import fields, validates, ValidationError
+
+class EmergencyContactSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = EmergencyContactModel
+        include_fk = True
+
+    created_at = fields.DateTime(time_format)
+    updated_at = fields.DateTime(time_format)
+
+    contact_number = fields.Nested("ContactNumberSchema")
+
+# TODO: use same custom validation from ContactNumberSchema?
+    @validates("contact_numbers")
+    def validate_contact_numbers(self, value):
+        if not ContactNumberModel.query.get(value):
+            raise ValidationError(f"{value} is not a valid contact number")
+
+    @validates("name")
+    def validate_name(self, value):
+        if not EmergencyContactModel.query.get(value):
+            raise ValidationError(f"{value} is not a valid contact number")
+
+
--- tests/unit/test_contact_number.py
+++ tests/unit/test_contact_number.py
@@ -1,14 +1,24 @@
 from models.contact_number import ContactNumberModel
+from schemas.contact_number import ContactNumberSchema
+from tests.unit.base_interface_test import BaseInterfaceTest
 
 emergency_contact_id = 1
 number = "503-291-9111"
 numtype = "Call"
 extension = "356"
 
-def test_contact_number():
-  contact_number = ContactNumberModel(emergency_contact_id, number, numtype, extension)
-  assert contact_number.emergency_contact_id == emergency_contact_id
-  assert contact_number.number == number
-  assert contact_number.numtype == numtype
-  assert contact_number.extension == extension
 
+class TestBaseContactNumberModel(BaseInterfaceTest):
+    def setup(self):
+        self.object = ContactNumberModel()
+        self.custom_404_msg = 'Lease not found'
+        self.schema = ContactNumberSchema
+
+
+class TestContactNumberModel():
+    def test_contact_number(self):
+        contact_number = ContactNumberModel(emergency_contact_id, number, numtype, extension)
+        assert contact_number.emergency_contact_id == emergency_contact_id
+        assert contact_number.number == number
+        assert contact_number.numtype == numtype
+        assert contact_number.extension == extension
