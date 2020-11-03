7728dc2d4acc1f48bb0b023c24bbb56e2d4c6ae3
cecillesalazar <cecillersalazar@gmail.com>
2020-11-02 20:52:25 -0800

    Removed test for notes - not in right file

--- tests/unit/test_ticket.py
+++ tests/unit/test_ticket.py
@@ -25,21 +25,3 @@ def test_ticket():
     assert test_ticket.status == testStatus
     assert test_ticket.urgency == testUrgency
     assert test_ticket.assignedUser == testAssignedUser
-
-
-def test_notes():
-    """The properties must match the notes model's inputs."""
-
-    testTicketId = 1
-    testText = 'Tenant has 40 cats'
-    testUser = 'user1 tester'
-
-    test_note = NotesModel(
-        ticketid=testTicketId,
-        text=testText,
-        user=testUser,
-    )
-
-    assert test_note.ticketid == testTicketId
-    assert test_note.text == testText
-    assert test_note.user == testUser
