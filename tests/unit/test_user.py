cdd7f38bc582a809c21ebddf0936e1faa9c52638
Nick Schimek <cfme00@gmail.com>
2020-11-12 21:51:30 +0900

    Remove user init method test
    This test doesn't really test anything that isn't tested in numerous
    places (the ability to create a user). It has a comment highlighting the
    fact that there is a dependency on the order of the arguments. This is
    an override of the default kwarg behavior provided by flask-restful. All
    init methods will be removed unless we need to have custom behavior
    which we will then preserve the kwarg behavior by calling super.

--- tests/unit/test_user.py
+++ tests/unit/test_user.py
@@ -4,17 +4,6 @@ import jwt
 import time
 from freezegun import freeze_time
 
-adminUserEmail = "user1@dwellingly.org"
-adminRole = RoleEnum.ADMIN
-phone = "1 800-Cal-Saul"
-
-def test_admin_user():
-    """The properties must match the model's inputs."""
-    admin_user = UserModel(email=adminUserEmail, password="1234", firstName="user1", lastName="admin", phone=phone, role=adminRole, archived=0)
-    assert admin_user.email == adminUserEmail
-    assert admin_user.role == adminRole
-    assert admin_user.phone == phone
-
 @patch.object(jwt, 'encode')
 def test_reset_password_token(stubbed_encode, app, test_database):
     user = UserModel.find_by_id(1)
