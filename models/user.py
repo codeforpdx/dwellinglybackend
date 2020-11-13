ac80f70915f45978d3f2a490ec2b1562f452ef3d
Nick Schimek <cfme00@gmail.com>
2020-11-13 21:48:19 +0900

    Create work factor env var (#144)
    * Create work factor config var

--- models/user.py
+++ models/user.py
@@ -1,3 +1,4 @@
+from flask import current_app
 from datetime import datetime, timedelta
 import bcrypt
 import time
@@ -33,16 +34,11 @@ class UserModel(BaseModel):
     lastActive = db.Column(db.DateTime, default=datetime.utcnow)
 
 
-    def __init__(self, firstName, lastName, email, password, phone, role, archived):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.email = email
-        self.phone = phone
-        self.hash_digest = UserModel.hash_pw(password)
+    def __init__(self, **kwargs):
+        super(UserModel, self).__init__(**kwargs)
+        self.hash_digest = UserModel.hash_pw(kwargs['password'])
         self._password = None
-        self.role = role
         self.archived = False
-        self.lastActive = datetime.utcnow()
 
     @property
     def password(self):
@@ -74,7 +70,7 @@ class UserModel(BaseModel):
 
     @staticmethod
     def hash_pw(plaintext_password):
-        return bcrypt.hashpw(bytes(plaintext_password, 'utf-8'), bcrypt.gensalt())
+        return bcrypt.hashpw(bytes(plaintext_password, 'utf-8'), bcrypt.gensalt(current_app.config['WORK_FACTOR']))
 
     def check_pw(self, plaintext_password):
         return bcrypt.checkpw(bytes(plaintext_password, 'utf-8'), self.hash_digest)
@@ -92,8 +88,8 @@ class UserModel(BaseModel):
             'created_at': Time.format_date(self.created_at),
             'updated_at': Time.format_date(self.updated_at)
         }
-    
-    def widgetJson(self, propertyName, date):          
+
+    def widgetJson(self, propertyName, date):
         return{
             'id': self.id,
             'stat': date,
@@ -108,12 +104,12 @@ class UserModel(BaseModel):
     @classmethod
     def find_by_role(cls, role):
         return cls.query.filter_by(role=role).all()
-    
+
     @classmethod
     def find_recent_role(cls, role, days):
         dateTime = datetime.utcnow() - timedelta(days = days)
         return db.session.query(UserModel).filter(UserModel.role == role).order_by(UserModel.created_at.desc()).limit(3).all()
-      
+
     @classmethod
     def find_by_role_and_name(cls, role, name):
         likeName = f'%{name}%'
--- tests/factory_fixtures/user.py
+++ tests/factory_fixtures/user.py
@@ -16,3 +16,20 @@ def create_property_manager():
         pm.save_to_db()
         return pm
     yield _create_property_manager
+
+
+@pytest.fixture
+def create_admin_user():
+    def _create_admin_user(firstName="Dwellingly", lastName="Admin"):
+        admin = UserModel(
+                email="admin@dwellingly.com",
+                password="asdf",
+                firstName=firstName,
+                lastName=lastName,
+                phone="505-503-1111",
+                role=RoleEnum.ADMIN,
+                archived=False
+            )
+        admin.save_to_db()
+        return admin
+    yield _create_admin_user
