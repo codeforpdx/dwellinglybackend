a077b966e672f8b00952f93e4a8cf504eaa61504
Gus Smith <guscomps@gmail.com>
2020-12-02 14:27:04 -0800

    Revert "Change find_by_name now that name is unique"
    This reverts commit 7aba65bb7be5c1116cecf52894e9752fc933f497.

--- models/property.py
+++ models/property.py
@@ -60,7 +60,7 @@ class PropertyModel(BaseModel):
 
     @classmethod
     def find_by_name(cls, name):
-        return cls.query.get(name=name)
+        return cls.query.filter_by(name=name).first()
 
     @classmethod
     def find_by_manager(cls, manager_id):
