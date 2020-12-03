6be44b4a527c35efb8f65bcd7735ab98cbd86c18
github username <souzajohnlee@gmail.com>
2020-12-02 17:21:43 -0800

    make invite email details clearer

--- resources/user_invite.py
+++ resources/user_invite.py
@@ -2,6 +2,7 @@ from flask_restful import Resource
 from flask import request
 from resources.admin_required import admin_required
 from models.user import UserModel, RoleEnum
+from scheams import UserRegisterSchemas
 from resources.email import Email
 import string
 import random
@@ -12,17 +13,9 @@ class UserInvite(Resource):
     def post(self):
         data = request.json
         temp_password = ''.join(random.choice(string.ascii_lowercase) for i in range(8))
-        user = UserModel(
-            email=data["email"],
-            firstName=data["firstName"],
-            lastName=data["lastName"],
-            phone=data["phone"],
-            password=temp_password,
-            role=RoleEnum.STAFF,
-            archived=False
-            )
-        if user.save_to_db():
-            Email.send_user_invite_msg(user)
-            return {"message": "User Invited"}, 200
-        else:
-            return {"message": "Could not create user"}, 400
+        user = UserModel.create(
+            schema=UserRegisterSchema,
+            payload={...request.json, "role": RoleEnum.STAFF, "archived": False, "password": temp_password}
+        ):
+        Email.send_user_invite_msg(user)
+        return {"message": "User Invited"}, 200
