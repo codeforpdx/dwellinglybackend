2bfa28a6f801c62029f7e7a9c4b5be3ee83f9231
Cecille Salazar <cecillersalazar@gmail.com>
2020-12-08 08:14:28 -0800

    Added tests for ticket validation methods

--- tests/unit/test_ticket.py
+++ tests/unit/test_ticket.py
@@ -7,3 +7,20 @@ class TestBaseTicketModel(BaseInterfaceTest):
         self.object = TicketModel()
         self.custom_404_msg = 'Ticket not found'
         self.schema = TicketSchema
+
+    def test_validate_tenant(self):
+        invalid_tenant = {
+            'tenantID': 666
+        }
+
+        invalid_tenant_validation_error = {'tenantID': ['666 is not a valid tenant ID']}
+        assert invalid_tenant_validation_error == TicketSchema().validate(invalid_tenant)
+
+    def test_validate_assigned_user_and_sender(self):
+        invalid_assigned_user_and_sender = {
+            'assignedUserID': 777,
+            'senderID': 888
+        }
+
+        invalid_assigned_user_validation_error = {'assignedUserID': ['777 is not a valid user ID'], 'senderID': ['888 is not a valid user ID']}
+        assert invalid_assigned_user_validation_error == TicketSchema().validate(invalid_assigned_user_and_sender)
