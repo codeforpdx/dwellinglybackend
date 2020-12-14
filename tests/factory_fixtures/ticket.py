3e5a01cf1ba6afa54077b9201df65c89b2bf0265
Steve <steve.s.yun@gmail.com>
2020-12-13 16:52:23 -0800

    added ticket fixture back

---
+++ tests/factory_fixtures/ticket.py
@@ -0,0 +1,25 @@
+import pytest
+from models.tickets import TicketModel
+from models.tickets import TicketStatus
+
+@pytest.fixture
+def ticket_attributes():
+    def _ticket_attributes(issue, tenant, assignedUser, sender):
+        return {
+            "issue": issue,
+            "tenant": tenant.id,
+            "assignedUser": assignedUser.id,
+            "sender": sender.id,
+            "status": TicketStatus.New,
+            "urgency": "high"
+        }
+    yield _ticket_attributes
+
+@pytest.fixture
+def create_ticket(ticket_attributes, create_tenant, create_admin_user, create_join_staff):
+    def _create_ticket(issue="Leaky pipe"):
+        tenant = create_tenant()
+        ticket = TicketModel(**ticket_attributes(issue, tenant, create_admin_user(), create_join_staff()))
+        ticket.save_to_db()
+        return ticket
+    yield _create_ticket
