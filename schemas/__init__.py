d52ee3a7a48f0da01bcaee27ffef4f10a20ef336
Trace Harris <traceh38@gmail.com>
2020-12-17 14:42:22 -0800

    input validations added in PropertySchema created PropertyAssignmentSchema… (#173)
    * input validations added in PropertySchema created PropertyAssignment Schema and tests: solves issue 394 all tests passing
    
    * cleaned up for style refactored unused import refactored commented out code

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,6 +1,7 @@
 from .lease import LeaseSchema
 from .tenant import TenantSchema
 from .property import PropertySchema
+from .property_assignment import PropertyAssignSchema
 from .user import *
 from .staff_tenants import StaffTenantSchema
 from .ticket import TicketSchema
