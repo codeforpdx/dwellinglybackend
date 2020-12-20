f5938633935040601e2824f9b82eb052d83a7693
Nick Schimek <cfme00@gmail.com>
2020-12-20 14:29:38 +0900

    Remove archived argument
    Passing an archived value is not necessary unless we want to specify
    that it should be True. By default it will be None in the column which
    results in Falsey.

--- resources/user_invite.py
+++ resources/user_invite.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource
 from flask import request
 from resources.admin_required import admin_required
-from models.user import UserModel, RoleEnum
+from models.user import UserModel
 from schemas import UserSchema
 from resources.email import Email
 import string
@@ -15,7 +15,7 @@ class UserInvite(Resource):
         temp_password = ''.join(random.choice(string.ascii_lowercase) for i in range(8))
         user = UserModel.create(
             schema=UserSchema,
-            payload={**data, "archived": False, "password": temp_password}
+            payload={**data, "password": temp_password}
         )
         Email.send_user_invite_msg(user)
         return {"message": "User Invited"}, 200
