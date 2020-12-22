8734e8377405daa367e77a8ae5aaf082648e8617
Nick Schimek <cfme00@gmail.com>
2020-12-22 19:10:21 +0900

    Refactor admin_authorized decorator

--- resources/admin_required.py
+++ resources/admin_required.py
@@ -3,12 +3,10 @@ from flask_jwt_extended import jwt_required, get_jwt_claims
 def admin_required(func):
     @jwt_required
     def check_admin_wrapper(*args, **kwargs):
-        #check if is_admin exist. If not discontinue function
-        claims = get_jwt_claims()
-        if not claims['is_admin']:
+        if get_jwt_claims()['is_admin']:
+            return func(*args, **kwargs)
+        else:
             return {'message': "Admin access required"}, 401
-
-        return func(*args, **kwargs)
 
     return check_admin_wrapper
 
