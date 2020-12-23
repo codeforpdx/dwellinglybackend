0c9fecabf809b60561e09869e5cb9b5a40390c29
Nick Schimek <cfme00@gmail.com>
2020-12-23 23:12:09 +0900

    Create post_load rule for property
    This will allow the property resource to bulk update the assigned
    managers.

--- schemas/property.py
+++ schemas/property.py
@@ -1,6 +1,8 @@
 from ma import ma
 from models.property import PropertyModel
-from marshmallow import fields, validates, ValidationError
+from models.user import UserModel
+from schemas.property_assignment import PropertyAssignSchema
+from marshmallow import fields, validates, ValidationError, post_load
 from utils.time import time_format
 
 
@@ -13,6 +15,15 @@ class PropertySchema(ma.SQLAlchemyAutoSchema):
     propertyManagerIDs = fields.List(fields.Integer(), required=True)
 
     @validates("propertyManagerIDs")
-    def validates_manager_assigned(self, value):
+    def validates_property_manager_ids(self, value):
         if len(value) < 1:
             raise ValidationError('manager must be assigned')
+
+        payload = [ { 'manager_id': value[manager_id] } for manager_id in range(0, len(value)) ]
+        PropertyModel.validate(PropertyAssignSchema, payload, partial=True, many=True)
+
+    @post_load
+    def make_property_attributes(self, data, **kwargs):
+        data['managers'] = [ UserModel.find(manager) for manager in data['propertyManagerIDs'] ]
+        del(data['propertyManagerIDs'])
+        return data
