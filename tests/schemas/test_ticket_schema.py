b66a38beb8abe7078bf36018b96742c10b800ed8
Steve Yun <steve.s.yun@gmail.com>
2021-01-14 03:04:34 +0000

    added validation for assignedUserID

--- tests/schemas/test_ticket_schema.py
+++ tests/schemas/test_ticket_schema.py
@@ -8,9 +8,21 @@ class TestTicketValidations:
 
         valid_payload = {
             "tenantID": create_tenant().id,
-            "assignedUserID": create_admin_user().id,
-            "senderID": create_join_staff().id,
+            "assignedUserID": create_join_staff().id,
+            "senderID": create_admin_user().id,
         }
 
         no_validation_errors = {}
         assert no_validation_errors == TicketSchema().validate(valid_payload)
+
+    def test_assign_non_staff(
+        self, empty_test_db, create_tenant, create_admin_user, create_join_staff
+    ):
+        invalid_assigned_user = {
+            "tenantID": create_tenant().id,
+            "assignedUserID": create_admin_user().id,
+            "senderID": create_join_staff().id,
+        }
+
+        validation_errors = TicketSchema().validate(invalid_assigned_user)
+        assert "assignedUserID" in validation_errors
