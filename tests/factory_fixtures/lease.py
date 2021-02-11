e5ef6ca1ea77bab10c2733096ddc3a12ed6658ab
Gus Smith <guscomps@gmail.com>
2021-02-10 20:36:13 -0800

    Unittest to check for active lease being returned

--- tests/factory_fixtures/lease.py
+++ tests/factory_fixtures/lease.py
@@ -4,15 +4,13 @@ from models.lease import LeaseModel
 
 @pytest.fixture
 def lease_attributes(faker):
-    def _lease_attributes(unitNum, tenant, property):
+    def _lease_attributes(unitNum, tenant, property, dateTimeStart, dateTimeEnd):
         return {
             "unitNum": unitNum,
             "tenantID": tenant.id,
             "propertyID": property.id,
-            "dateTimeStart": faker.date_time_this_decade(),
-            "dateTimeEnd": faker.date_time_this_decade(
-                before_now=False, after_now=True
-            ),
+            "dateTimeStart": dateTimeStart,
+            "dateTimeEnd": dateTimeEnd,
             "occupants": faker.random_number(digits=2),
         }
 
@@ -21,11 +19,23 @@ def lease_attributes(faker):
 
 @pytest.fixture
 def create_lease(faker, lease_attributes, create_property, create_tenant):
-    def _create_lease(tenant=None, property=None, unitNum=None):
+    def _create_lease(
+        tenant=None,
+        property=None,
+        unitNum=None,
+        dateTimeStart=None,
+        dateTimeEnd=None,
+    ):
         unitNum = unitNum or faker.building_number()
         tenant = tenant or create_tenant()
         property = property or create_property()
-        lease = LeaseModel(**lease_attributes(unitNum, tenant, property))
+        dateTimeStart = dateTimeStart or faker.date_time_this_decade()
+        dateTimeEnd = dateTimeEnd or faker.date_time_this_decade(
+            before_now=False, after_now=True
+        )
+        lease = LeaseModel(
+            **lease_attributes(unitNum, tenant, property, dateTimeStart, dateTimeEnd)
+        )
         lease.save_to_db()
         return lease
 
--- tests/unit/test_tenant.py
+++ tests/unit/test_tenant.py
@@ -1,7 +1,9 @@
+from models.property import PropertyModel
 import pytest
 from tests.unit.base_interface_test import BaseInterfaceTest
 from models.tenant import TenantModel
 from schemas.tenant import TenantSchema
+import datetime
 
 
 class TestBaseTenantModel(BaseInterfaceTest):
@@ -9,6 +11,28 @@ class TestBaseTenantModel(BaseInterfaceTest):
         self.object = TenantModel()
         self.custom_404_msg = "Tenant not found"
         self.schema = TenantSchema
+
+    def test_only_active_lease_returned(self, faker, create_lease, create_tenant):
+        tenant = create_tenant()
+
+        # Create an expired lease for the tenant
+        lease_expired_end = faker.date_time_this_decade(
+            before_now=True, after_now=False
+        )
+        lease_expired_start = lease_expired_end - datetime.timedelta(days=365)
+        lease_expired = create_lease(
+            tenant=tenant,
+            dateTimeStart=lease_expired_start,
+            dateTimeEnd=lease_expired_end,
+        )
+
+        # Create an active lease for the tenant
+        lease_active = create_lease(
+            tenant=tenant, property=PropertyModel.find_by_id(lease_expired.propertyID)
+        )
+
+        # Active lease should be the only one that shows up
+        assert tenant.json()["lease"] == lease_active.json()
 
 
 @pytest.mark.usefixtures("empty_test_db")
