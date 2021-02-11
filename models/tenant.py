cc4d84aa8b4624502603e2a904a728510cbca13c
Gus Smith <guscomps@gmail.com>
2021-02-10 19:39:08 -0800

    Get first active lease in tenant json()
    lazy="dynamic" is required so that we can filter() over leases in
    json(). See: https://stackoverflow.com/questions/11578070

--- models/tenant.py
+++ models/tenant.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import relationship
 from db import db
 from models.base_model import BaseModel
 from utils.time import Time
+from models.lease import LeaseModel
 
 
 class TenantModel(BaseModel):
@@ -16,17 +17,19 @@ class TenantModel(BaseModel):
     # relationships
     staff = relationship("UserModel", secondary="staff_tenant_links")
     leases = db.relationship(
-        "LeaseModel", backref="tenant", lazy=True, cascade="all, delete-orphan"
+        "LeaseModel", backref="tenant", lazy="dynamic", cascade="all, delete-orphan"
     )
 
     def json(self):
+        first_active_lease = self.leases.filter(LeaseModel.active()).first()
+        active_lease_json = first_active_lease.json() if first_active_lease else ""
         return {
             "id": self.id,
             "firstName": self.firstName,
             "lastName": self.lastName,
             "fullName": "{} {}".format(self.firstName, self.lastName),
             "phone": self.phone,
-            "lease": self.leases[0].json() if self.leases else "",
+            "lease": active_lease_json,
             "staff": [user.json() for user in self.staff] if self.staff else [],
             "created_at": Time.format_date(self.created_at),
             "updated_at": Time.format_date(self.updated_at),
