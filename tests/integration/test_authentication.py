dc766a14f245a9d6abc3248e8e9e76a7f9a99363
Gus Smith <guscomps@gmail.com>
2021-03-10 19:07:34 -0800

    Move auth tests from mailers

--- tests/integration/test_authentication.py
+++ tests/integration/test_authentication.py
@@ -199,3 +199,22 @@ class TestLeaseAuthorizations:
             f"/api/lease/{lease.id}", json=payload, headers=admin_header
         )
         assert response.status_code == 200
+
+
+@pytest.mark.usefixtures("client_class", "empty_test_db")
+class TestEmailAuthorizations:
+    def setup(self):
+        self.endpoint = "/api/user/message"
+
+    def test_auth_header_is_required(self):
+        response = self.client.post(self.endpoint)
+
+        assert is_valid(response, 401)
+        assert response.json == {"message": "Missing authorization header"}
+
+    def test_all_roles_except_admin_are_denied_access(self, auth_headers):
+        payload = {"user_id": 1, "subject": "Some email subject", "body": "Some body"}
+        for role, token in auth_headers.items():
+            if role != "admin":
+                response = self.client.post(self.endpoint, json=payload, headers=token)
+                assert is_valid(response, 401)
