3f119cd117a8d188bfc2dbeba135492832820f11
aedwardg <aedwardg1@gmail.com>
2021-03-28 15:41:41 -0700

    Add unit test for `PropertyModel.tenants()`
    Unit test added to check that only tenants with active leases are
    added to the list in `PropertyModel.tenants()`

--- tests/unit/test_property.py
+++ tests/unit/test_property.py
@@ -2,6 +2,8 @@ import pytest
 from tests.unit.base_interface_test import BaseInterfaceTest
 from models.property import PropertyModel
 from schemas.property import PropertySchema
+from utils.time import Time
+from dateutil.relativedelta import relativedelta
 
 
 class TestBasePropertyModel(BaseInterfaceTest):
@@ -46,3 +48,22 @@ class TestPropertyModel:
         tenant = lease.tenant
 
         assert property.json(include_tenants=True)["tenants"] == [tenant.json()]
+
+    def test_only_active_tenants_included(self, create_lease):
+        active_lease = create_lease()
+
+        old_lease_end = Time._yesterday()
+        old_lease_start = old_lease_end - relativedelta(years=1)
+        expired_lease = create_lease(
+            property=active_lease.property,
+            dateTimeStart=old_lease_start,
+            dateTimeEnd=old_lease_end,
+        )
+
+        property = PropertyModel.find(expired_lease.propertyID)
+        property_json = property.json()
+
+        assert len(property_json["tenants"]) == 1
+        assert len(property_json["lease"]) == 2
+        assert active_lease.tenant.json() in property_json["tenants"]
+        assert expired_lease.tenant.json() not in property_json["tenants"]
