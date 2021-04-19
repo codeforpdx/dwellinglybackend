3a02661865a5e4e41c42dcbfafb9df1ef065e668
Nick Schimek <19519317+NickSchimek@users.noreply.github.com>
2021-04-19 19:33:13 +0900

    Refactor tests

--- tests/integration/test_properties.py
+++ tests/integration/test_properties.py
@@ -1,8 +1,49 @@
 import pytest
 import json
+from unittest.mock import patch
+from schemas.property import PropertySchema
 from models.property import PropertyModel
-from models.user import UserModel
-from models.tenant import TenantModel
+
+
+@pytest.mark.usefixtures("client_class", "empty_test_db")
+class TestProperty:
+    def setup(self):
+        self.endpoint = "/api/properties/"
+
+    def test_get(self, valid_header, create_property):
+        property = create_property()
+        response = self.client.get(
+            f"{self.endpoint}{property.id}", headers=valid_header
+        )
+
+        assert response.json == property.json(include_tenants=True)
+
+    def test_delete(self, valid_header, create_property):
+        with patch.object(PropertyModel, "delete") as mock_delete:
+            response = self.client.delete(f"{self.endpoint}1", headers=valid_header)
+
+        mock_delete.assert_called_once_with(1)
+        assert response.status_code == 200
+        assert response.json == {"message": "Property deleted"}
+
+    def test_put(self, valid_header, create_property):
+        property = create_property()
+
+        with patch.object(
+            PropertyModel, "update", return_value=property
+        ) as mock_update:
+            response = self.client.put(
+                f"{self.endpoint}1", json={"num_units": 2}, headers=valid_header
+            )
+
+        mock_update.assert_called_once_with(
+            schema=PropertySchema,
+            id=1,
+            context={"name": property.name},
+            payload={"num_units": 2},
+        )
+        assert response.status_code == 200
+        assert response.json == property.json()
 
 
 def test_get_properties(client, test_database):
@@ -30,32 +71,6 @@ def test_post_property(
         "message": {"name": ["A property with this name already exists"]}
     }
     assert response.status_code == 400
-
-
-def test_get_property_by_id(client, auth_headers, test_database):
-    """The get property by name returns a successful response code."""
-    response = client.get("/api/properties/1", headers=auth_headers["admin"])
-    property_info = response.get_json()
-    user_json = UserModel.find(5).json()
-    assert response.status_code == 200
-    assert property_info["name"] == "test1"
-    assert property_info["address"] == "123 NE FLanders St"
-    assert property_info["num_units"] == 5
-    assert property_info["city"] == "Portland"
-    assert property_info["state"] == "OR"
-    assert property_info["zipcode"] == "97207"
-    assert property_info["propertyManagers"] == [user_json]
-    assert property_info["archived"] == 0
-    assert property_info["tenants"] == [TenantModel.find(1).json()]
-
-    """
-    The server responds with an error if the URL contains a non-existent property id
-    """
-    responseBadPropertyName = client.get(
-        "/api/properties/23", headers=auth_headers["admin"]
-    )
-    assert responseBadPropertyName == 404
-    assert responseBadPropertyName.json == {"message": "Property not found"}
 
 
 def test_archive_property_by_id(client, auth_headers, create_property, test_database):
@@ -139,68 +154,3 @@ class TestPropertyArchivalMethods:
             "message": "Property IDs missing in request"
         }
         assert responseBadPropertyID.status_code == 400
-
-
-def test_delete_property_by_id(client, auth_headers, test_database):
-    test_property = PropertyModel.find_by_name("The Reginald")
-
-    """First verify that the property exists"""
-    response = client.get(
-        f"/api/properties/{test_property.id}", headers=auth_headers["admin"]
-    )
-    assert response.status_code == 200
-
-    response = client.delete(
-        f"/api/properties/{test_property.id}", headers=auth_headers["admin"]
-    )
-    assert response.status_code == 200
-    assert response.json == {"message": "Property deleted"}
-
-    """Now verify that the property no longer exists"""
-    response = client.get(
-        f"/api/properties/{test_property.id}", headers=auth_headers["admin"]
-    )
-    assert response.status_code == 404
-    assert response.json == {"message": "Property not found"}
-
-    """The server responds with a 404 error if property not exist"""
-    response = client.delete("/api/properties/23", headers=auth_headers["admin"])
-    assert response == 404
-
-
-def test_update_property(client, empty_test_db, valid_header, create_property):
-    test_property = create_property()
-    new_property_address = "123 NE Flanders St"
-
-    property_json = test_property.json()
-
-    property_json["address"] = new_property_address
-
-    """
-    Updating property info should be successful when the payload includes
-    the property's current name. The response JSON should reflect these changes.
-    """
-    response = client.put(
-        f"/api/properties/{test_property.id}",
-        json=property_json,
-        headers=valid_header,
-    )
-    assert response == 200
-    assert response.json == property_json
-
-    new_property = create_property()
-    duplicate_name = test_property.name
-
-    new_property_json = new_property.json()
-    new_property_json["name"] = duplicate_name
-
-    """
-    The server responds with a 400 error when attempting to update a
-    property with the name of another existing property.
-    """
-    response = client.put(
-        f"/api/properties/{new_property.id}",
-        json=new_property_json,
-        headers=valid_header,
-    )
-    assert response == 400
