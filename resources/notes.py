62459bdcb687d2ed372d919bf36d3f6529a3d5b5
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-04-26 11:01:52 -0700

    create notes endpoint pulls ticketid and userid from request

--- resources/notes.py
+++ resources/notes.py
@@ -1,18 +1,19 @@
-from flask_restful import Resource, reqparse
+from flask_restful import Resource
+from flask import request
 from models.notes import NotesModel
 from utils.authorizations import pm_level_required
 from schemas.notes import NotesSchema
+from flask_jwt_extended import get_jwt_identity
 
 
 class Notes(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument("text")
-    parser.add_argument("authorID")
-
     @pm_level_required
     def post(self, id):
-        data = Notes.parser.parse_args()
-        note_payload = {"ticketid": id, "text": data.text, "userid": data.authorID}
-
-        created_note = NotesModel.create(schema=NotesSchema, payload=note_payload)
-        return created_note.json()
+        return NotesModel.create(
+            schema=NotesSchema,
+            payload={
+                "text": request.json["text"],
+                "ticketid": id,
+                "userid": get_jwt_identity(),
+            },
+        ).json()
--- tests/integration/test_notes.py
+++ tests/integration/test_notes.py
@@ -6,18 +6,17 @@ invalidTicketID = 777
 
 
 def test_notes_POST(client, auth_headers):
-    newNote = {"text": "We don't need no water", "authorID": 5}
+    newNote = {"text": "We don't need no water"}
 
     response = client.post(
         f"{endpoint}/{validTicketID}/notes", json=newNote, headers=auth_headers["admin"]
     )
-
     assert is_valid(response, 200)
     assert response.json["id"] != 0
     assert response.json["id"] == 5
     assert response.json["ticketid"] == 1
     assert response.json["text"] == "We don't need no water"
-    assert response.json["user"] == "Gray Pouponn"
+    assert response.json["user"] == "user4 admin"
 
     response = client.post(
         f"{endpoint}/{invalidTicketID}/notes",
