523aedb6ba423fe5c578427761692ed28511f600
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-05-26 14:24:15 -0700

    create note fixture no longer needs parameters

--- tests/factory_fixtures/notes.py
+++ tests/factory_fixtures/notes.py
@@ -11,11 +11,13 @@ def note_attributes():
 
 
 @pytest.fixture
-def create_note(faker, note_attributes):
-    def _create_note(userid, ticketid, text=None):
-        if not text:
-            text = faker.paragraph()
-        note = NotesModel(**note_attributes(text, userid, ticketid))
+def create_note(faker, note_attributes, create_admin_user, create_ticket):
+    def _create_note(user=None, ticket=None, text=None):
+        text = text or faker.paragraph()
+        user = user or create_admin_user()
+        ticket = ticket or create_ticket()
+
+        note = NotesModel(**note_attributes(text, user, ticket))
         note.save_to_db()
         return note
 
