2a66e1d5c6732ccad73252cf20ac2c0a25405ba5
Paul Stevens <paul.stevens.developer@gmail.com>
2021-05-31 16:22:55 -0700

    refactors tenant unit tests

--- tests/unit/test_tenant.py
+++ tests/unit/test_tenant.py
@@ -1,9 +1,8 @@
-from models.property import PropertyModel
 import pytest
 from tests.unit.base_interface_test import BaseInterfaceTest
 from models.tenant import TenantModel
 from schemas.tenant import TenantSchema
-import datetime
+from utils.time import Time
 
 
 class TestBaseTenantModel(BaseInterfaceTest):
@@ -12,30 +11,22 @@ class TestBaseTenantModel(BaseInterfaceTest):
         self.custom_404_msg = "Tenant not found"
         self.schema = TenantSchema
 
-    def test_only_active_lease_returned(self, faker, create_lease, create_tenant):
-        tenant = create_tenant()
-
-        # Create an expired lease for the tenant
-        lease_expired_end = faker.date_time_this_decade(
-            before_now=True, after_now=False
-        )
-        lease_expired_start = lease_expired_end - datetime.timedelta(days=365)
-        lease_expired = create_lease(
-            tenant=tenant,
-            dateTimeStart=lease_expired_start,
-            dateTimeEnd=lease_expired_end,
-        )
-
-        # Create an active lease for the tenant
-        lease_active = create_lease(
-            tenant=tenant, property=PropertyModel.find(lease_expired.propertyID)
-        )
-
-        # Active lease should be the only one that shows up
-        assert tenant.json()["lease"] == lease_active.json()
-
 
 @pytest.mark.usefixtures("empty_test_db")
-class TestTenantFactory:
-    def test_create_tenant(self, create_tenant):
-        assert create_tenant()
+class TestTenantModel:
+    def test_json(self, create_tenant, create_lease):
+        tenant = create_tenant()
+        lease = create_lease(tenant=tenant)
+
+        assert tenant.json() == {
+            "id": tenant.id,
+            "firstName": tenant.firstName,
+            "lastName": tenant.lastName,
+            "fullName": "{} {}".format(tenant.firstName, tenant.lastName),
+            "phone": tenant.phone,
+            "staff": tenant.staff,
+            "lease": lease.json(),
+            "created_at": Time.format_date(tenant.created_at),
+            "updated_at": Time.format_date(tenant.updated_at),
+            "archived": tenant.archived,
+        }
