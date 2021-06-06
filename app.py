085d1df247135faf5d9cae1aea6631c2e0b6752c
Nick Schimek <19519317+NickSchimek@users.noreply.github.com>
2021-06-06 19:23:17 +0900

    More refactoring - Moves routes into separate file (#307)
    * Move environment configs into config folder
    
    * Move routes to separate config file

--- app.py
+++ app.py
@@ -1,78 +1,24 @@
 from flask import Flask
-from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from flask_mailman import Mail
 from models.user import UserModel
 from models.revoked_tokens import RevokedTokensModel
-from resources.user import (
-    UserRegister,
-    User,
-    UserLogin,
-    ArchiveUser,
-    UsersRole,
-    UserAccessRefresh,
-    UserRoles,
-    Users,
-)
-from resources.user_invite import UserInvite
-from resources.users.pending_users import UsersPending
-from resources.reset_password import ResetPassword
-from resources.property import Properties, Property, ArchiveProperty, ArchiveProperties
-from resources.staff_tenants import StaffTenants
-from resources.tenants import Tenants, Tenant
-from resources.emergency_contacts import EmergencyContacts
-from resources.email import Email
-from resources.tickets import Ticket, Tickets
-from resources.notes import Note
-from resources.lease import Lease, Leases
-from resources.widgets import Widgets
 from db import db
 from ma import ma
 from manage import dbsetup
-from config import app_config
-
-
-def create_routes(app):
-    api = Api(app, prefix="/api/")
-    api.add_resource(UserRegister, "register")
-    api.add_resource(Property, "properties/<int:id>")
-    api.add_resource(Properties, "properties")
-    api.add_resource(ArchiveProperties, "properties/archive")
-    api.add_resource(ArchiveProperty, "properties/archive/<int:id>")
-    api.add_resource(User, "user/<int:user_id>")
-    api.add_resource(UserInvite, "user/invite")
-    api.add_resource(Users, "user")
-    api.add_resource(UsersRole, "users/role")
-    api.add_resource(UsersPending, "users/pending")
-    api.add_resource(ArchiveUser, "user/archive/<int:user_id>")
-    api.add_resource(UserLogin, "login")
-    api.add_resource(UserRoles, "roles")
-    api.add_resource(Widgets, "widgets")
-    api.add_resource(Email, "user/message")
-    api.add_resource(UserAccessRefresh, "refresh")
-    api.add_resource(StaffTenants, "staff-tenants")
-    api.add_resource(Tenants, "tenants")
-    api.add_resource(Tenant, "tenants/<int:id>")
-    api.add_resource(
-        EmergencyContacts, "emergencycontacts", "emergencycontacts/<int:id>"
-    )
-    api.add_resource(Lease, "lease/<int:id>")
-    api.add_resource(Leases, "lease")
-    api.add_resource(Tickets, "tickets")
-    api.add_resource(Ticket, "tickets/<int:id>")
-    api.add_resource(Note, "tickets/<int:id>/notes")
-    api.add_resource(ResetPassword, "reset-password", "reset-password/<string:token>")
+from config.environment import app_environments
+from config.routes import Routes
 
 
 def create_app(env):
     app = Flask(__name__)
 
-    app.config.from_object(app_config[env])
+    app.config.from_object(app_environments[env])
     app.register_blueprint(dbsetup)
 
-    # declare the available routes
-    create_routes(app)
+    # Define the available routes
+    Routes.routing(app)
 
     # allow cross-origin (CORS)
     CORS(app, origins=app.config["CORS_ORIGINS"])
