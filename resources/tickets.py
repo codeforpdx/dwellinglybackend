c1a95e1c4a01b912f8cabc8f11f640b0e32ae314
aedwardg <aedwardg1@gmail.com>
2021-06-16 19:16:24 -0700

    Update Tickets GET resource

--- resources/tickets.py
+++ resources/tickets.py
@@ -1,7 +1,6 @@
 from db import db
 from flask_restful import Resource
 from models.tickets import TicketModel
-from models.tenant import TenantModel
 from utils.authorizations import pm_level_required
 from flask import request
 from schemas.ticket import TicketSchema
@@ -27,12 +26,10 @@ class Ticket(Resource):
 class Tickets(Resource):
     @pm_level_required
     def get(self):
-        if request.args and request.args["tenant_id"]:
-            return {
-                "tickets": TenantModel.find(request.args["tenant_id"]).tickets.json()
-            }
-        else:
-            return {"tickets": TicketModel.query.json()}
+        tickets = TicketModel.query
+        if "tenant_id" in request.args:
+            tickets = tickets.where(TicketModel.tenant_id == request.args["tenant_id"])
+        return {"tickets": tickets.json()}
 
     @pm_level_required
     def post(self):
