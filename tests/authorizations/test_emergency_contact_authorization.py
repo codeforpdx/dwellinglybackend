8496fbc665e9d92a132c285fc23eb99101c5704e
MO <27942892+ovsyankimo@users.noreply.github.com>
2021-06-23 19:57:23 -0700

    running linters

--- tests/authorizations/test_emergency_contact_authorization.py
+++ tests/authorizations/test_emergency_contact_authorization.py
@@ -28,32 +28,40 @@ class TestEmergenyContactsAuthorizations:
         assert is_valid(response, 401)
         assert response.json == {"message": "Missing authorization header"}
 
-
     def test_prop_manager_denied_emerg_contacts_POST(self, pm_header):
-        response = self.client.post(self.endpoint, json = self.newContact, headers=pm_header)
+        response = self.client.post(
+            self.endpoint, json=self.newContact, headers=pm_header
+        )
         assert is_valid(response, 401)  # UNAUTHORIZED - Admin Access Required
-
 
     def test_prop_manager_denied_emerg_contacts_DELETE(self, pm_header):
-        response = self.client.delete(f"{self.endpoint}/{self.user_id}", headers=pm_header)
+        response = self.client.delete(
+            f"{self.endpoint}/{self.user_id}", headers=pm_header
+        )
         assert is_valid(response, 401)  # UNAUTHORIZED - Admin Access Required
-
 
     def test_staff_denied_emerg_contacts_POST(self, staff_header):
-        response = self.client.post(self.endpoint, json=self.newContact, headers=staff_header)
+        response = self.client.post(
+            self.endpoint, json=self.newContact, headers=staff_header
+        )
         assert is_valid(response, 401)  # UNAUTHORIZED - Admin Access Required
-
 
     def test_staff_denied_emerg_contacts_DELETE(self, staff_header):
-        response = self.client.delete(f"{self.endpoint}/{self.user_id}", headers=staff_header)
+        response = self.client.delete(
+            f"{self.endpoint}/{self.user_id}", headers=staff_header
+        )
         assert is_valid(response, 401)  # UNAUTHORIZED - Admin Access Required
 
-
     def test_admin_allowed_emerg_contacts_POST(self, admin_header):
-        response = self.client.post(self.endpoint, json=self.newContact, headers=admin_header)
+        response = self.client.post(
+            self.endpoint, json=self.newContact, headers=admin_header
+        )
         assert is_valid(response, 201)
 
-
-    def test_admin_allowed_emerg_contacts_DELETE(self, admin_header, create_emergency_contact):
-        response = self.client.delete(f"{self.endpoint}/{create_emergency_contact().id}", headers=admin_header)
+    def test_admin_allowed_emerg_contacts_DELETE(
+        self, admin_header, create_emergency_contact
+    ):
+        response = self.client.delete(
+            f"{self.endpoint}/{create_emergency_contact().id}", headers=admin_header
+        )
         assert is_valid(response, 200)
