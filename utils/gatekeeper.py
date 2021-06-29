bdd06f1288b5f61398515525712f21d717c2f411
aedwardg <aedwardg1@gmail.com>
2021-06-29 02:06:30 -0700

    Add gatekeeper utility to check request params
    Use the `allowed_params` decorator to restrict an endpoint to only
    accept certain fields in the JSON request.
    
    Decorator takes a set of strings as its only argument.

---
+++ utils/gatekeeper.py
@@ -0,0 +1,33 @@
+from flask import current_app, request
+from functools import wraps
+from typing import Set
+
+
+def allowed_params(params: Set[str]):
+    def decorator(endpoint):
+        @wraps(endpoint)
+        def verify_fields(*args, **kwargs):
+            if not (request.json.keys() <= params):
+                return invalid_field_error(params)
+            return endpoint(*args, **kwargs)
+
+        return verify_fields
+
+    return decorator
+
+
+def invalid_field_error(field_set):
+    if current_app.env == "development":
+        raise GatekeeperError(field_set)
+    else:
+        return {"message": "Invalid request field"}, 400
+
+
+class GatekeeperError(Exception):
+    def __init__(self, field_set, message="Invalid request field"):
+        self.field_set = field_set
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"{self.message}: request fields must be one or more of {self.field_set}"
