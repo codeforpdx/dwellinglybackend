5141f70db996602b5abce8f79e75813aac2a6c20
Nick Schimek <19519317+NickSchimek@users.noreply.github.com>
2021-07-04 21:20:22 +0900

    Refactor property resource and tests (#320)

--- resources/property.py
+++ resources/property.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource, reqparse
+from flask_restful import Resource
 from flask import request
 from utils.authorizations import admin_required
 from db import db
@@ -6,7 +6,30 @@ from models.property import PropertyModel
 from schemas.property import PropertySchema
 
 
+class Property(Resource):
+    @admin_required
+    def get(self, id):
+        return PropertyModel.find(id).json(include_tenants=True)
+
+    @admin_required
+    def delete(self, id):
+        PropertyModel.delete(id)
+        return {"message": "Property deleted"}
+
+    @admin_required
+    def put(self, id):
+        property = PropertyModel.find(id)
+
+        return PropertyModel.update(
+            schema=PropertySchema,
+            id=id,
+            context={"name": property.name},
+            payload=request.json,
+        ).json()
+
+
 class Properties(Resource):
+    @admin_required
     def get(self):
         return {"properties": PropertyModel.query.json()}
 
@@ -35,44 +58,17 @@ class ArchiveProperty(Resource):
 
 
 class ArchiveProperties(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument("ids", action="append")
-
     @admin_required
     def patch(self):
-        data = ArchiveProperties.parser.parse_args()
-        propertyList = []
-
-        if not ("ids" in data and type(data["ids"]) is list):
+        if not ("ids" in request.json and type(request.json["ids"]) is list):
             return {"message": "Property IDs missing in request"}, 400
 
-        for id in data["ids"]:
+        properties = []
+        for id in request.json["ids"]:
             property = PropertyModel.find(id)
             property.archived = True
-            propertyList.append(property)
+            properties.append(property)
 
-        db.session.bulk_save_objects(propertyList)
+        db.session.bulk_save_objects(properties)
         db.session.commit()
-        return {"properties": [p.json() for p in PropertyModel.query.all()]}, 200
-
-
-class Property(Resource):
-    @admin_required
-    def get(self, id):
-        return PropertyModel.find(id).json(include_tenants=True)
-
-    @admin_required
-    def delete(self, id):
-        PropertyModel.delete(id)
-        return {"message": "Property deleted"}
-
-    @admin_required
-    def put(self, id):
-        property = PropertyModel.find(id)
-
-        return PropertyModel.update(
-            schema=PropertySchema,
-            id=id,
-            context={"name": property.name},
-            payload=request.json,
-        ).json()
+        return {"properties": PropertyModel.query.json()}
