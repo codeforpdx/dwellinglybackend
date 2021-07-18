d19d3b750741941be55f7719c906c439439ec70e
hugh <609799548@qq.com>
2021-07-18 23:08:16 +0800

    update revoked_token id field type

--- models/revoked_tokens.py
+++ models/revoked_tokens.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 class RevokedTokensModel(BaseModel):
     __tablename__ = "revoked_tokens"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(36), primary_key=True)
     jti = db.Column(db.String(120), nullable=False)
 
     @classmethod
