a94fa78fa67e8ffaf84f5ef837f270307c82df60
Nick Schimek <19519317+NickSchimek@users.noreply.github.com>
2021-07-19 23:22:59 +0900

    Monkeypatch env setting for tests
    An autouse fixture is defines the Flask environment for the testing
    environment for tests that do not use the app context. For tests that do
    need an app context then it also needs to be defined before the app
    loads which is done in the app method.
    
    https://docs.pytest.org/en/6.2.x/reference.html#monkeypatch

--- config/environment.py
+++ config/environment.py
@@ -39,7 +39,6 @@ class Development(Default):
 
 
 class Testing(Default):
-    os.environ["FLASK_ENV"] = "testing"
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("TEST_DATABASE_URL") or "sqlite://"
     WORK_FACTOR = 4
