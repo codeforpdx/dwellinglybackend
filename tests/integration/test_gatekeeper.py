5dcab215f75b9c3d7679ede93d2a8155440ccdc6
aedwardg <aedwardg1@gmail.com>
2021-07-26 12:05:32 -0700

    Remove `request` import and fix `put()` call

--- tests/integration/test_gatekeeper.py
+++ tests/integration/test_gatekeeper.py
@@ -1,5 +1,5 @@
 import pytest
-from flask import current_app, request
+from flask import current_app
 from tests.factory_fixtures.dummy_resource import DummyResource
 from utils.gatekeeper import GatekeeperError
 
@@ -9,14 +9,14 @@ class TestGatekeeper:
     def test_empty_set_allows_no_params(self):
         with pytest.raises(GatekeeperError):
             with current_app.test_request_context(json={"hi": "there"}):
-                DummyResource.put(request)
+                DummyResource().put()
 
     def test_raises_error_for_unspecified_params(self):
         DummyResource.dummy_params.update({"hello", "world"})
 
         with pytest.raises(GatekeeperError) as error_info:
             with current_app.test_request_context(json={"hi": "there"}):
-                DummyResource.put(request)
+                DummyResource().put()
         assert "Invalid request field" in str(error_info.value)
 
         DummyResource.dummy_params.clear()
@@ -24,4 +24,4 @@ class TestGatekeeper:
     def test_subset_of_allowed_params_is_okay(self):
         DummyResource.dummy_params.update({"hello", "world", "again"})
         with current_app.test_request_context(json={"hello": "there"}):
-            DummyResource.put(request)
+            DummyResource().put()
