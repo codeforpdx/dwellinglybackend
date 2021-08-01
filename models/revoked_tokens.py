1582d020f9d19605083c19a97fc1ee9d88bf77cc
Nick Schimek <19519317+NickSchimek@users.noreply.github.com>
2021-08-02 06:32:34 +0900

    Update jti query to search on jti column instead of id

--- models/revoked_tokens.py
+++ models/revoked_tokens.py
@@ -5,9 +5,10 @@ from models.base_model import BaseModel
 class RevokedTokensModel(BaseModel):
     __tablename__ = "revoked_tokens"
 
-    id = db.Column(db.String(36), primary_key=True)
-    jti = db.Column(db.String(120), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    jti = db.Column(db.String(36), nullable=False)
 
     @classmethod
-    def is_jti_blacklisted(cls, jti):
-        return bool(cls.query.get(jti))
+    def is_jti_revoked(cls, jti):
+        token = cls.query.filter_by(jti=jti).scalar()
+        return token is not None
